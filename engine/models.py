a51ccece6730c1335c17ce62498b2034717bf335
Toby <tdudleyoffice@gmail.com>
2019-08-22 17:26:00 -0500

    calculates no. of seconds as float since last use and prints to screen

--- engine/models.py
+++ engine/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 
 
-class Player(models.Model):
-    id = models.IntegerField # Maybe rehash as a foreign key of User object
-    gold = models.IntegerField(default=200)
-    lumber = models.IntegerField(default=200)
-    stone = models.IntegerField(default=200)
-    score = models.IntegerField(default=1)
-    last_action = models.DateTimeField(auto_now=False)
-
-    def __str__(self):
-        return f'{self.id} Gold: {self.gold} Lumber: {self.lumber} Stone: {self.stone} Score: {self.score}'
-
-
 class Building(models.Model):
     name = models.CharField(max_length=64)
     max_level = models.IntegerField(default=20)
@@ -33,3 +21,17 @@ class Building(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.level})'
+
+
+class Player(models.Model):
+    id = models.IntegerField # Maybe rehash as a foreign key of User object
+    gold = models.IntegerField(default=200)
+    lumber = models.IntegerField(default=200)
+    stone = models.IntegerField(default=200)
+    score = models.IntegerField(default=1)
+    last_action = models.DateTimeField(auto_now=False)
+    buildings = models.ManyToManyField(Building, blank=True)
+
+    def __str__(self):
+        return f'{self.id} Gold: {self.gold} Lumber: {self.lumber} \
+                  Stone: {self.stone} Score: {self.score}'
--- engine/views.py
+++ engine/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from engine.models import Building, Player
 from django.http import HttpResponse
+
+from django.utils import timezone
+import time
 # Create your views here.
 
 def index(request):
     buildings = Building.objects.all()
     player = Player.objects.get(pk=1)
-    return render(request, 'engine/index.html', {"player": player, "buildings": buildings})
+    seconds_passed = (timezone.now() - player.last_action).total_seconds()
+    player.last_action = timezone.now()
+    player.save()
+    return render(request, 'engine/index.html', {"player": player, "buildings": buildings, "seconds_passed": seconds_passed})
