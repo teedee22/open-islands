359befcfcf1100bd8d10b14e63e38372ba167bfc
Toby <tdudleyoffice@gmail.com>
2019-08-22 16:25:20 -0500

    some basic models set up for player and building

--- engine/models.py
+++ engine/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    id = models.IntegerField # Maybe rehash as a foreign key of User object
+    gold = models.IntegerField(default=200)
+    lumber = models.IntegerField(default=200)
+    stone = models.IntegerField(default=200)
+    score = models.IntegerField(default=1)
+    last_action = models.DateTimeField(auto_now=False)
+
+    def __str__(self):
+        return f'{self.id} Gold: {self.gold} Lumber: {self.lumber} Stone: {self.stone} Score: {self.score}'
+
+
+class Building(models.Model):
+    name = models.CharField(max_length=64)
+    max_level = models.IntegerField(default=20)
+    level = models.IntegerField(default=0)
+    cost = models.IntegerField(default=13)
+
+    def output(self):
+        if self.level > 0:
+            return 13 * self.level
+        else:
+            return 13
+
+    def cost_gold(self):
+        if self.level > 0:
+            return 50 * self.level
+        else:
+            return 50
+
+    def __str__(self):
+        return f'{self.name} ({self.level})'
--- engine/views.py
+++ engine/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from engine.models import Building, Player
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    buildings = Building.objects.all()
+    player = Player.objects.get(pk=1)
+    return render(request, 'engine/index.html', {"player": player, "buildings": buildings})
